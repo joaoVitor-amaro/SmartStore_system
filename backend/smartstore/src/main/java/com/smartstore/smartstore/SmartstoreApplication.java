597aeda0830e465212b53ac74bcb25d90635b8d6
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-20 23:48:56 -0300

    first commit

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/SmartstoreApplication.java
@@ -0,0 +1,13 @@
+package com.smartstore.smartstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartstoreApplication.class, args);
+	}
+
+}
---
+++ backend/smartstore/src/test/java/com/smartstore/smartstore/SmartstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartstore.smartstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
