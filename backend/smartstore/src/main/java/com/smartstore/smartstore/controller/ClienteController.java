caf143846a18a796406c83a2095eb4afc095f105
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-23 17:28:57 -0300

    feat: Adicionado o Cross para fazer requisição com o react

--- backend/smartstore/src/main/java/com/smartstore/smartstore/controller/ClienteController.java
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/controller/ClienteController.java
@@ -6,13 +6,11 @@ import com.smartstore.smartstore.response.ApiResponse;
 import com.smartstore.smartstore.service.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ClienteController {
     private ClienteService clienteService;
 
