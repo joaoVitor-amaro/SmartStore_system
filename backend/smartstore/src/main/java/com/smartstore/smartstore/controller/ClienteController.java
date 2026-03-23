c0066914c5a54cd43e342c82404706fe125f2772
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-23 15:07:37 -0300

    feat: Criado o controle com a endpoint de cadastrar o cliente

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/controller/ClienteController.java
@@ -0,0 +1,35 @@
+package com.smartstore.smartstore.controller;
+
+import com.smartstore.smartstore.dto.UserCreateRequestDto;
+import com.smartstore.smartstore.dto.UserCreateResponseDto;
+import com.smartstore.smartstore.response.ApiResponse;
+import com.smartstore.smartstore.service.ClienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+    private ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<ApiResponse<UserCreateResponseDto>> cadastrarCliente(@RequestBody UserCreateRequestDto userCreateRequest) {
+        UserCreateResponseDto data = this.clienteService.cadastroCliente(userCreateRequest);
+        ApiResponse<UserCreateResponseDto> response = new ApiResponse<>(
+          true,
+                "Usuário cadastrado",
+                data,
+                null
+
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
