f91cddc3f2bdd87475fe0e97c747a2ba267598cc
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-26 17:00:30 -0300

    feat: Adicionar o controller com o endpoint para enviar as categorias

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/controller/CategoriaController.java
@@ -0,0 +1,27 @@
+package com.smartstore.smartstore.controller;
+
+import com.smartstore.smartstore.model.Categoria;
+import com.smartstore.smartstore.service.CategoriaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categorias")
+@CrossOrigin(origins = "*")
+public class CategoriaController {
+    private CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> listar() {
+        return ResponseEntity.ok(categoriaService.listAll());
+    }
+}
