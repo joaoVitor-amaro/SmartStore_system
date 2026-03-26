f633062e20e60839e4932029eef2113d7dfb9713
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-26 11:04:14 -0300

    feat: Adicionado o repositorio para acessar as tabelas Marca e Categoria no banco de dados

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/repository/MarcaRepository.java
@@ -0,0 +1,8 @@
+package com.smartstore.smartstore.repository;
+
+import com.smartstore.smartstore.model.Marca;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarcaRepository extends JpaRepository<Marca, Long> {
+}
+    
