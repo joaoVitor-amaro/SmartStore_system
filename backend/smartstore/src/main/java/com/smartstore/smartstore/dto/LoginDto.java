51a220c007ce2e9994897ceebe776fa52c39b9e9
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-27 00:36:13 -0300

    feat: add o request do login

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/dto/LoginDto.java
@@ -0,0 +1,14 @@
+package com.smartstore.smartstore.dto;
+
+public class LoginDto {
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
