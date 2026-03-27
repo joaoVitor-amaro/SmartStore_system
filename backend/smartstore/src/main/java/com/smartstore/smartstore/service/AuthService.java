7d9e636e7f94bd34fd5d169cc15d372f97df2125
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-27 00:52:09 -0300

    feat: implementa AuthService com validação de login e geração de token JWT

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/service/AuthService.java
@@ -0,0 +1,37 @@
+package com.smartstore.smartstore.service;
+
+import com.smartstore.smartstore.dto.AuthResponseDto;
+import com.smartstore.smartstore.dto.LoginDto;
+import com.smartstore.smartstore.model.Cliente;
+import com.smartstore.smartstore.repository.ClienteRepository;
+import com.smartstore.smartstore.security.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private ClienteRepository clienteRepository;
+    private JwtService jwtService;
+    private PasswordEncoder passwordEncoder;
+
+    public AuthService(ClienteRepository clienteRepository, JwtService jwtService, PasswordEncoder passwordEncoder) {
+        this.clienteRepository = clienteRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public AuthResponseDto login(LoginDto loginDto) {
+        Cliente cliente = clienteRepository
+                .findByEmail(loginDto.getEmail())
+                .orElseThrow(() ->
+                    new IllegalArgumentException("Cliente não encontrado")
+                );
+        boolean senhaValida = passwordEncoder.matches(loginDto.getSenha(), cliente.getSenha());
+        if (!senhaValida) {
+            throw new IllegalArgumentException("Senha inválida");
+        }
+        String token = jwtService.gerarToken(cliente.getEmail());
+        return new AuthResponseDto(token);
+    }
+}
+
