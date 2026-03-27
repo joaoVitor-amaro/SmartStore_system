21c820fb6400e829c2a087c432c64af3c0168c41
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-27 00:49:07 -0300

    feat: adiciona configuração global de CORS para permitir requisições do frontend

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.smartstore.smartstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.addAllowedOrigin("http://localhost:5173");
+
+        config.addAllowedHeader("*");
+
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
