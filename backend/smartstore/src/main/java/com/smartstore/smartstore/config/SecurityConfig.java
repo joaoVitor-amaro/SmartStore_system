43194eaa4d06cbf76584a73190620fdac551f59f
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-27 09:46:01 -0300

    feat: configurar sessão stateless no SecurityConfig

--- backend/smartstore/src/main/java/com/smartstore/smartstore/config/SecurityConfig.java
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,10 +26,12 @@ public class SecurityConfig {
         http
                 .cors(cors -> {}) // habilita CORS
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/login",
-                                "/auth/register",
                                 "/cliente/cadastro",
                                 "/api/categorias",
                                 "/produtos/home",
