2a2ad2970e8df2b0926786776c4d299d541afdc8
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-28 01:51:59 -0300

    feat: Dto para response da endpoint de cadastrar o produto

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/dto/ProdutoCreateResponseDto.java
@@ -0,0 +1,69 @@
+package com.smartstore.smartstore.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProdutoCreateResponseDto {
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private Integer estoque;
+    private String fabricadoEm;
+
+    private MultipartFile imagemUrl;
+
+    private Long marcaId;
+    private Long categoriaId;
+
+    private String email;
+
+    public ProdutoCreateResponseDto() {
+    }
+
+    public ProdutoCreateResponseDto(ProdutoCreateRequestDto produtoDto, String emai) {
+        this.nome = produtoDto.getNome();
+        this.descricao = produtoDto.getDescricao();
+        this.preco = produtoDto.getPreco();
+        this.estoque = produtoDto.getEstoque();
+        this.fabricadoEm = produtoDto.getFabricadoEm();
+        this.imagemUrl = produtoDto.getImagem();
+        this.marcaId = produtoDto.getMarcaId();
+        this.categoriaId = produtoDto.getCategoriaId();
+        this.email = emai;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public String getFabricadoEm() {
+        return fabricadoEm;
+    }
+
+    public MultipartFile getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public Long getMarcaId() {
+        return marcaId;
+    }
+
+    public Long getCategoriaId() {
+        return categoriaId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
