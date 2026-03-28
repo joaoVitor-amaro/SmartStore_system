201aa90e2c4c9d80e369ccbfb4140c20b27e2559
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-28 01:50:54 -0300

    feat: Adicionar o endpoint de cadastro do produto

--- backend/smartstore/src/main/java/com/smartstore/smartstore/controller/ProdutoController.java
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/controller/ProdutoController.java
@@ -1,7 +1,13 @@
 package com.smartstore.smartstore.controller;
 
+import com.smartstore.smartstore.dto.ProdutoCreateRequestDto;
+import com.smartstore.smartstore.dto.ProdutoCreateResponseDto;
 import com.smartstore.smartstore.dto.ProdutoHomeResponseDto;
+import com.smartstore.smartstore.response.ApiResponse;
 import com.smartstore.smartstore.service.ProdutoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +26,19 @@ public class ProdutoController {
     @GetMapping("/home")
     public List<ProdutoHomeResponseDto> listarProdutosHome() {
         return produtoService.listarProdutosHome();
+    }
+
+
+    @PostMapping(value = "/cadastro", consumes = "multipart/form-data")
+    public ResponseEntity<ApiResponse<ProdutoCreateResponseDto>> cadastrarProduto(
+            @ModelAttribute ProdutoCreateRequestDto produtoDto, Authentication authentication) throws Exception {
+        ProdutoCreateResponseDto data = produtoService.cadastrar(produtoDto, authentication.getName());
+        ApiResponse<ProdutoCreateResponseDto> response = new ApiResponse<>(
+                true,
+                "Usuário cadastrado",
+                data,
+                null
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
