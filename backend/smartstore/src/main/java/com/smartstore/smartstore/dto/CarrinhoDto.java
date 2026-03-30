6b8c631db5f02932b52734c5cf21aab4c8423a7b
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-30 01:45:58 -0300

    feat: Response dos produtos no carrinho do cliente para confirmar o pedido

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/dto/CarrinhoDto.java
@@ -0,0 +1,16 @@
+package com.smartstore.smartstore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarrinhoDto {
+    private Long idCarrinho;
+    private List<ItemCarrinhoDto> itens;
+    private Double total;
+}
