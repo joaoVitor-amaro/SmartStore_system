dd6fdb15d58a21f1df433fab9112fafd395cb3e4
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-04-01 02:16:47 -0300

    feat: adicionar o metodo de procurar o cliente e o status do seu carrinho

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/repository/CompraRepository.java
@@ -0,0 +1,9 @@
+package com.smartstore.smartstore.repository;
+
+import com.smartstore.smartstore.model.Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompraRepository extends JpaRepository<Compra, Long> {
+}
