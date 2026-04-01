dd4aa249b5f8e5da5005e3153d99e8bbf3550bce
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-04-01 02:11:01 -0300

    feat: Criar endpoint para o cliente confirmar o pagamento

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/controller/CompraController.java
@@ -0,0 +1,32 @@
+package com.smartstore.smartstore.controller;
+
+import com.smartstore.smartstore.dto.CompraDTO;
+import com.smartstore.smartstore.response.ApiResponse;
+import com.smartstore.smartstore.service.CompraService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/compra")
+@CrossOrigin(origins = "*")
+public class CompraController {
+    private final CompraService compraService;
+
+    public CompraController(CompraService compraService) {
+        this.compraService = compraService;
+    }
+
+    @PostMapping("/finalizar")
+    public ResponseEntity<ApiResponse<?>> finalizarCompra(@RequestBody CompraDTO dto, Authentication authentication) {
+        Long compraId = compraService.realizarCompra(authentication.getName(), dto);
+        return ResponseEntity.ok(new ApiResponse<>(
+                true,
+                "Compra realizada com sucesso!",
+                Map.of("compraId", compraId, "status", "APROVADO"),
+                null
+        ));
+    }
+}
