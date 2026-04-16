6f67ed50858e2ff699d0f714e69c1988f9f946ca
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-04-16 17:37:11 -0300

    feat: Adicionar o request de recuperar e redefinir a senha

---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/dto/NovaSenhaRequestDto.java
@@ -0,0 +1,14 @@
+package com.smartstore.smartstore.dto;
+
+public class NovaSenhaRequestDto {
+    private String token;
+    private String senha;
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
---
+++ backend/smartstore/src/main/java/com/smartstore/smartstore/dto/RecuperarSenhaRequest.java
@@ -0,0 +1,9 @@
+package com.smartstore.smartstore.dto;
+
+public class RecuperarSenhaRequest {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+}
